b516f42e0afd39525e3a3e7b341a673e834c1b88
libo1160777981 <18186537257@163.com>
2016-07-29 23:32:02 +0800

    like java

---
+++ src/test/java/com/libo/AppTest.java
@@ -0,0 +1,14 @@
+package com.libo;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+
+	@Test
+	public void testApp() {
+		System.out.println("12345678");
+	}
+}
